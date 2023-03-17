849b781cfbedf1719fb6b3b41eb2d864a5bd1460
Anubhav Mishra <omanubhavmishra@gmail.com>
2023-03-18 02:49:06 +0530

    Sealing of a number(Method 2)

--- DSA PRACTICE/src/com/anubhav/day1/Question_5.java
+++ DSA PRACTICE/src/com/anubhav/day1/Question_5.java
@@ -4,10 +4,10 @@ public class Question_5 {
 
 	public static void main(String[] args) {
 		int[] arr = {-10,0,2,6,12,48,59,66};
-		int target = 15;
-		int ans = sealingOfNumber(arr, target);
+		int target = 743;
+		int ans = sealingOfNumber2(arr, target);
 		if(ans!=-1) {
-			System.out.println(ans);
+			System.out.println(arr[ans]);
 		}
 		else
 		{
@@ -15,6 +15,7 @@ public class Question_5 {
 		}
 		
 	}
+	//Meathod 1
 	static int sealingOfNumber(int[] arr , int target) {
 		int start = 0;
 		int doesNotFound = 0;
@@ -46,5 +47,31 @@ public class Question_5 {
 			}
 		}
 		return doesNotFound;
+	}
+	//Method 2
+	static int sealingOfNumber2(int[] arr , int target) {
+		int start = 0;
+		int end = arr.length-1;
+		while(start <= end) {
+			if(target > arr[end]) {
+				return -1;
+			}
+			//Get the middle element
+			int mid = start + (end - start)/2;
+//			System.out.println("Start: "+start+" End: "+end+" Mid: "+ mid+" Element in the middle: "+arr[mid]+" Target: "+target);
+			if(target < arr[mid]) {
+				end = mid -1 ;
+			}
+			else if(target > arr[mid]) {
+				start = mid + 1;
+			}
+			else
+			{
+				//ans found
+				return mid+1;
+			}
+		}
+		//ans does not found
+		return start;
 	}
 }
